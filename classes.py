56019611bd71fb7460f8c5041c6d5f6307758c5e
Duncan Mazza <dmazza@olin.edu>
2019-03-09 18:56:00 -0500

    Fix problem with downloading of a book & markov func

--- classes.py
+++ classes.py
@@ -23,17 +23,18 @@ class InvalidBookError(Error):
 class Book:
     """
     A book object stores all relevant information about the book (identified by
-    book_name_author) and provides methods for processing each book's data.
+    name_author) and provides methods for processing each book's data.
 
     All attributes:
-    self.book_name_author - "Book Title, by Author Name"
+    self.name_author - "Book Title, by Author Name"
     self.path_to_book - "books/path_to_book_text_file"
-    self.tokenized_book_file_path - "books/path_to_the_book_token_file.txt"
-    self.tokenized_book - book parsed into a list of words
-    self.book_hist_file_path - "books/path_to_the_book_hist_file.txt"
-    self.book_hist - histogram of book's word usage
+    self.words_file_path - "books/path_to_the_book_token_file.txt"
+    self.words - book parsed into a list of words
+    self.hist_file_path - "books/path_to_the_hist_file.txt"
+    self.hist - histogram of book's word usage
     """
-    def __init__(self, book_name_author, gutenberg_index):
+
+    def __init__(self, name_author, gutenberg_index, override_existing_download=False):
         """
         Downloads the book from project gutenberg if the book is in
         gutenberg_index. The link to the book is generated from the book's
@@ -42,43 +43,50 @@ class Book:
         downloaded (and override_existing_download == False), returns true if
         the book was successfully downloaded.
         """
-        self.book_name_author = book_name_author
+        self.name_author = name_author
 
-        book_file_path = "books/{}.txt".format(self.book_name_author)
+        book_file_path = "books/{}.txt".format(self.name_author)
+        try:
+            self.path_to_book
+        except AttributeError:
+            self.path_to_book = book_file_path
 
         # Check if the book already exists and override it if indicated.
         if os.path.exists(book_file_path):
             if override_existing_download == False:
                 print("Book file already exists")
-                raise InvalidBookError
+                return None
             else:
                 print("Overriding existing download: deleting {}".format(self.book_file_path))
                 os.system("rm -rf {}".format(self.book_file_path))
 
         try:
-            book_number = gutenberg_index[self.book_name_author]
+            book_number = gutenberg_index[self.name_author]
         except KeyError:
             print("Book name/author not in the index.")
             raise InvalidBookError
 
         book_link_number = ''
-        for i in range(len(book_number)):
+        for i in range(1,len(book_number))  :
             # generating the unique part of the download link for the book
             book_link_number = book_link_number + book_number[0:i] + "/"
-        book_link_number = book_link_number + book_file_path
-
+        book_link_number = book_link_number + book_number + "/" + book_number + ".txt"
         try:
-            print("Downloading {}".format(self.book_name_author))
+            print("Downloading {}".format(self.name_author))
             book_text = requests.get("http://mirrors.xmission.com/gutenberg/{}".format(book_link_number)).text
+            if book_text[0:6] == "<html>": # some links require a different format which is handled below
+                book_link_number = book_link_number[0:len(book_link_number)-4]+"-0.txt"
+                book_text = requests.get("http://mirrors.xmission.com/gutenberg/{}".format(book_link_number)).text
         except requests.exceptions.MissingSchema:
             print("Invalid url / could not download from this link")
             raise InvalidBookError
 
+        book_text = book_text[2000:len(book_text)-2000]
+
         book_file = open(book_file_path, 'w')
         book_file.write(book_text)
         book_file.close()
-        self.path_to_book = book_file_path
-        print("Successfully downloaded {} and wrote to file".format(self.book_name_author))
+        print("Successfully downloaded {} and wrote to file".format(self.name_author))
 
     def tokenize_book(self):
         """
@@ -92,9 +100,10 @@ class Book:
             words = pickle.loads(file_text)
             file.close()
             try:
-                self.tokenized_book
+                self.words
             except AttributeError:
-                self.tokenized_book = words
+                self.words = words
+            self.length = len(words)
             return words
         else:
             # Information for parsing the book
@@ -102,11 +111,11 @@ class Book:
             whitespace_list = list(string.whitespace)
             delete_list = punc_list + whitespace_list
 
-            print("Tokenizing {}".format(self.book_name_author))
+            print("Tokenizing {}".format(self.name_author))
             lines = []
             words = []
 
-            with open("books/"+self.book_name_author+".txt") as text:
+            with open("books/"+self.name_author+".txt") as text:
                 for line in text:
                     processed_line = line.strip()
                     lines.append(processed_line)
@@ -122,11 +131,11 @@ class Book:
             data_text = pickle.dumps(words)
             file.write(data_text)
             file.close()
-            print('Successfully tokenized and wrote file for {}'.format(self.book_name_author))
+            print('Successfully tokenized and wrote file for {}'.format(self.name_author))
 
-            self.tokenized_book_file_path = data_file_name
-            self.tokenized_book = words
-            self.book_length = len(words)
+            self.words_file_path = data_file_name
+            self.words = words
+            self.length = len(words)
             return words
 
     def make_hist(self):
@@ -138,25 +147,25 @@ class Book:
         if not os.path.exists(hist_file_path):
             words = self.tokenize_book
             hist = {}
-            for word in words:
+            for word in self.words:
                 hist[word] = hist.get(word,0) + 1
 
             hist_text = pickle.dumps(hist)
             hist_file = open(hist_file_path, 'wb')
             hist_file.write(hist_text)
             hist_file.close()
-            self.book_hist_file_path = hist_file_path
-            self.book_hist = hist
-            print("Histogram for {} successfully made and written to file".format(self.book_name_author))
+            self.hist_file_path = hist_file_path
+            self.hist = hist
+            print("Histogram for {} successfully made and written to file".format(self.name_author))
             return hist
         else:
             file = open(hist_file_path, 'rb')
             file_text = file.read()
             hist = pickle.loads(file_text)
             try:
-                self.book_hist
+                self.hist
             except AttributeError:
-                self.book_hist = hist
+                self.hist = hist
             return hist
 
     def make_atf(self):
@@ -170,7 +179,7 @@ class Book:
         """
         atf_file_path = self.path_to_book[:len(self.path_to_book)-4]+"___atf.txt"
         if not os.path.exists(atf_file_path):
-            hist = self.book_hist
+            hist = self.hist
             max_word_ct = 0
             for word in hist:
                 if hist[word] > max_word_ct:
@@ -185,9 +194,9 @@ class Book:
             atf_text = pickle.dumps(atf)
             atf_file.write(atf_text)
             atf_file.close()
-            self.book_atf_file_path = atf_file_path
-            self.book_atf = atf
-            print("Histogram for {} successfully made and written to file".format(self.book_name_author))
+            self.atf_file_path = atf_file_path
+            self.atf = atf
+            print("Histogram for {} successfully made and written to file".format(self.name_author))
             return atf
         else:
             file = open(atf_file_path, 'rb')
@@ -195,94 +204,87 @@ class Book:
             atf = pickle.loads(file_text)
             file.close()
             try:
-                self.book_atf
+                self.atf
             except AttributeError:
-                self.book_atf = atf
+                self.atf = atf
             return atf
 
     def make_random_markov_helper(self):
-        random_rmf_file_path = self.path_to_book[:len(self.path_to_book)-4]+"___rmf.txt"
-        if not os.path.exists(random_rmf_file_path):
-            words = self.tokenize_book
-            hist = self.book_hist
-
-            print("Making helper dictionary for markov chain for {}".format(self.book_name_author))
-            random_rmf = {}
-            for i in range(len(words)-1):
+        random_markov_file_path = self.path_to_book[:len(self.path_to_book)-4]+"___randommarkov.txt"
+        if not os.path.exists(random_markov_file_path):
+            print("Making helper dictionary for markov chain for {}".format(self.name_author))
+            random_markov = {}
+            for i in range(len(self.words)-1):
                 try:
-                    random_rmf[words[i]].append(words[i+1])
+                    random_markov[self.words[i]].append(self.words[i+1])
                 except KeyError:
-                    random_rmf[words[i]] = [words[i+1]]
+                    random_markov[self.words[i]] = [self.words[i+1]]
 
-            random_rmf_text = pickle.dumps(random_rmf)
-            random_rmf_file = open(random_rmf_file_path, 'wb')
-            random_rmf_file.write(random_rmf_text)
-            random_rmf_file.close()
-            self.book_random_rmf_file_path = random_rmf_file_path
-            self.book_random_rmf = random_rmf
-            print("Helper dictionary for markov chain for {} successfully made and written to file".format(self.book_name_author))
-            return random_rmf
+            random_markov_text = pickle.dumps(random_markov)
+            random_markov_file = open(random_markov_file_path, 'wb')
+            random_markov_file.write(random_markov_text)
+            random_markov_file.close()
+            self.random_markov_file_path = random_markov_file_path
+            self.random_markov = random_markov
+            print("Helper dictionary for markov chain for {} successfully made and written to file".format(self.name_author))
+            return random_markov
         else:
-            file = open(random_rmf_file_path, 'rb')
+            file = open(random_markov_file_path, 'rb')
             file_text = file.read()
-            random_rmf = pickle.loads(file_text)
+            random_markov = pickle.loads(file_text)
             file.close()
             try:
-                self.book_random_rmf
+                self.random_markov
             except AttributeError:
-                self.book_random_rmf = random_rmf
-            return random_rmf
+                self.random_markov = random_markov
+            return random_markov
 
     def make_assisted_markov_helper(self):
-        if not os.path.exists(assisted_rmf_file_path):
-            words = self.tokenize_book
-            hist = self.book_hist
-
-            print("Making helper dictionary for markov chain for {}".format(self.book_name_author))
-            assisted_rmf = {}
-            for i in range(len(words)-1):
+        assisted_markov_file_path = self.path_to_book[:len(self.path_to_book)-4]+"___assistedmarkov.txt"
+        if not os.path.exists(assisted_markov_file_path):
+            print("Making helper dictionary for markov chain for {}".format(self.name_author))
+            assisted_markov = {}
+            for i in range(len(self.words)-1):
                 try:
-                    if words[i+1] not in assisted_rmf[words[i]]:
-                        for i in range(self.book_atf[words[i+1]]):
-                            assisted_rmf[words[i]].append(words[i+1])
+                    assisted_markov[self.words[i]]
                 except KeyError:
-                    assisted_rmf[words[i]] = []
-                    if words[i+1] not in assisted_rmf[words[i]]:
-                        for i in range(self.book_atf[words[i+1]]):
-                            assisted_rmf[words[i]].append(words[i+1])
-
-            assisted_rmf_text = pickle.dumps(assisted_rmf)
-            assisted_rmf_file = open(assisted_rmf_file_path, 'wb')
-            assisted_rmf_file.write(assisted_rmf_text)
-            assisted_rmf_file.close()
-            self.book_assisted_rmf_file_path = assisted_rmf_file_path
-            self.book_assisted_rmf = assisted_rmf
-            print("Helper dictionary for markov chain for {} successfully made and written to file".format(self.book_name_author))
-            return assisted_rmf
+                    assisted_markov[self.words[i]] = []
+                if self.words[i+1] not in assisted_markov[self.words[i]]:
+                    for j in range(int(100*self.atf[self.words[i+1]])):
+                        assisted_markov[self.words[i]].append(self.words[i+1])
+            assisted_markov_text = pickle.dumps(assisted_markov)
+            assisted_markov_file = open(assisted_markov_file_path, 'wb')
+            assisted_markov_file.write(assisted_markov_text)
+            assisted_markov_file.close()
+            self.assisted_markov_file_path = assisted_markov_file_path
+            self.assisted_markov = assisted_markov
+            print("Helper dictionary for markov chain for {} successfully made and written to file".format(self.name_author))
+            return assisted_markov
         else:
-            file = open(assisted_rmf_file_path, 'rb')
+            file = open(assisted_markov_file_path, 'rb')
             file_text = file.read()
-            assisted_rmf = pickle.loads(file_text)
+            assisted_markov = pickle.loads(file_text)
             file.close()
             try:
-                self.assisted_rmf
+                self.assisted_markov
             except AttributeError:
-                self.assisted_rmf = assisted_rmf
-            return assisted_rmf
+                self.assisted_markov = assisted_markov
+            return assisted_markov
 
     def make_book(self, gutenberg_index):
         """
         Runs all Book methods in one go.
         """
-
-        words = self.tokenize_book()
-        hist = self.make_hist(words)
-        atf = self.make_atf(hist)
+        self.tokenize_book()
+        self.make_hist()
+        self.make_atf()
+        self.make_random_markov_helper()
+        self.make_assisted_markov_helper()
 
     def __str__(self):
         return "Book: {}\nBook file: {}\nBook token file: {}\nBook hist\
-        file: {}".format(self.book_name_author, self.book_file_path,
-        self.tokenized_book_file_path, self.book_hist_file_path)
+        file: {}".format(self.name_author, self.book_file_path,
+        self.words_file_path, self.hist_file_path)
 
 # class MarkovChainGenerator:
 #     def __init__(self, book1_obj, book2_obj):
