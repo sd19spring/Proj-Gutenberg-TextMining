2125097aa0dd202980fce1bc0beff5133b4eba4a
Duncan Mazza <dmazza@olin.edu>
2019-03-10 15:42:06 -0400

    Restructure markov chain generation and analysis

--- automatic_text_mining.py
+++ automatic_text_mining.py
@@ -1,34 +1,18 @@
-################################################################################
-
 """
 This software will take a list of books (book_list) and automatically find,
 download, and generate all associated files for the books.
 """
 
-################################################################################
-
-import random
-import requests
-import os
-import classes as cl
 import utility_functions as uf
 import pickle
 
-################################################################################
-
-# book_list = [
-# ('Frankenstein, by Mary Wollstonecraft (Godwin) Shelley'),
-# ('A Tale of Two Cities, by Charles Dickens'),
-# ('The Adventures of Sherlock Holmes, by Arthur Conan Doyle'),
-# ('Adventures of Huckleberry Finn, by Mark Twain'),
-# ('The Yellow Wallpaper, by Charlotte Perkins Gilman'),
-# ('Metamorphosis, by Franz Kafka')
-# ]
-
 book_list = [
-('Frankenstein, by Mary Wollstonecraft (Godwin) Shelley'),
-('A Tale of Two Cities, by Charles Dickens'),
+    ('Frankenstein, by Mary Wollstonecraft (Godwin) Shelley'),
+    ('A Tale of Two Cities, by Charles Dickens'),
 ]
+
+len_markov_chain = 1000
+num_sets_markov_chains = 3 # must be a multiple of 3
 
 uf.check_GUTINDEX()
 uf.check_books_folder()
@@ -41,10 +25,27 @@ gutenberg_index_file.close()
 
 library = uf.handle_books(gutenberg_index)
 
-for book_name_author in library:
-    control_markov = uf.control_markov_chain(library[book_name_author])
-    print("\nControl markov chain for {}:\n{}".format(book_name_author,control_markov))
-    random_markov = uf.random_markov_chain(library[book_name_author])
-    print("\nRandom markov chain for {}:\n{}\n".format(book_name_author,random_markov))
-    assisted_markov = uf.assisted_markov_chain(library[book_name_author])
-    print("\nAssisted markov chain for {}:\n{}\n".format(book_name_author,assisted_markov))
+# for book_name_author in library:
+#     control_markov = uf.control_markov_chain(library[book_name_author])
+#     print("\nControl markov chain for {}:\n{}".format(book_name_author,control_markov))
+#     random_markov = uf.random_markov_chain(library[book_name_author])
+#     print("\nRandom markov chain for {}:\n{}\n".format(book_name_author,random_markov))
+#     assisted_markov = uf.assisted_markov_chain(library[book_name_author])
+#     print("\nAssisted markov chain for {}:\n{}\n".format(book_name_author,assisted_markov))
+
+text_lists = []
+for i in range(num_sets_markov_chains):
+    if i < num_sets_markov_chains/3:
+        text_lists.append(
+            uf.control_markov_chain(library['Frankenstein, by Mary Wollstonecraft (Godwin) Shelley'], len_markov_chain))
+    elif num_sets_markov_chains/3 <= i < 2*num_sets_markov_chains/3:
+        text_lists.append(
+            uf.random_markov_chain(library['Frankenstein, by Mary Wollstonecraft (Godwin) Shelley'], len_markov_chain))
+
+
+    else:
+        text_lists.append(
+            uf.assisted_markov_chain(library['Frankenstein, by Mary Wollstonecraft (Godwin) Shelley'], len_markov_chain))
+
+matrix = uf.make_similarity_matrix(text_lists)
+uf.display_similarity_matrix(matrix)
