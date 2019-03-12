6f39ab908b29a6466c4fbfc983af3f8c73328831
Duncan Mazza <dmazza@olin.edu>
2019-03-11 22:36:47 -0400

    Add ability to handle either 1 or two texts

--- automatic_text_mining.py
+++ automatic_text_mining.py
@@ -8,11 +8,15 @@ import pickle
 
 book_list = [
     ('Frankenstein, by Mary Wollstonecraft (Godwin) Shelley'),
-    ('Such is Life, by Frank Wedekind'),
+    ('Watersprings, by Arthur Christopher Benson'),
 ]
 
-len_markov_chain = 20000
-num_sets_markov_chains = 30 # must be a multiple of 3
+# Parameters for program
+len_markov_chain = 30000  # desired length in words of a markov chain
+num_sets_markov_chains = 6  # number of markov chains generated per text; must be a multiple of 3
+num_texts_plot = uf.get_num_texts_plot()  # plot the similarity matrix of either 1 text or two texts. If only one text is considered either
+# user will choose to input only one text, or opt to use the hard coded list, in which only the first text will be
+# considered
 
 uf.check_GUTINDEX()
 uf.check_books_folder()
@@ -23,7 +27,7 @@ gutenberg_index_text = gutenberg_index_file.read()
 gutenberg_index = pickle.loads(gutenberg_index_text)
 gutenberg_index_file.close()
 
-library = uf.handle_books(gutenberg_index)
+library = uf.handle_books(gutenberg_index, num_texts_plot)
 
 print("\nExample markov chains:\n")
 
@@ -34,22 +38,28 @@ for book_name_author in library:
     print("\nRandom markov chain for {}:\n{}\n".format(book_name_author,uf.list_to_string(random_markov)))
     assisted_markov = uf.assisted_markov_chain(library[book_name_author])
     print("\nAssisted markov chain for {}:\n{}\n".format(book_name_author,uf.list_to_string(assisted_markov)))
+    if num_texts_plot == 1:
+        break
 
 text_lists = []
 for book in book_list:
     for i in range(num_sets_markov_chains):
-        if num_sets_markov_chains/3 <= i < 2*num_sets_markov_chains/3:
+        if num_sets_markov_chains/3 < i:
+            text_lists.append(
+                uf.control_markov_chain(library[book], len_markov_chain))
+        elif num_sets_markov_chains/3 <= i < 2*num_sets_markov_chains/3:
             text_lists.append(
                 uf.random_markov_chain(library[book], len_markov_chain))
         else:
             text_lists.append(
                 uf.assisted_markov_chain(library[book], len_markov_chain))
-    break
+    if num_texts_plot == 1:
+        break
 
-# For testing the similarity matrix calculation, uncomment the following line of
-# code. There should be two points plotted on top of each other and one separated
-# a small distance from them. 
+"""For testing the similarity matrix calculation, uncomment the following line of
+code. There should be two points plotted on top of each other and one separated
+a small distance from them."""
 # text_lists = [['this','is','a','test'],['this','is','a','test'],['this','is','not','a','test']]
 
 matrix = uf.make_similarity_matrix(text_lists)
-uf.display_similarity_matrix(matrix)
+uf.display_similarity_matrix(matrix, num_sets_markov_chains, num_texts_plot)
